7a2e8c6b5b3afd72b1686713faeea5f0af4c7fde
Marcin Nowicki <pr0d1r2@gmail.com>
2015-05-22 14:20:29 +0200

    Add some cosmetic changes.

--- youtube-dl-gui.rb
+++ youtube-dl-gui.rb
@@ -2,9 +2,11 @@ class YoutubeDlGui < Formula
   homepage "https://github.com/MrS0m30n3/youtube-dl-gui"
   url "https://github.com/MrS0m30n3/youtube-dl-gui.git",
     :revision => "36c8147a0ba4da01215d92957246527285cca710"
+
+  # cannot use git tag right now as new version with OSX support not released
   version "0.3.7"
 
-  depends_on "python"
+  depends_on :python if MacOS.version <= :snow_leopard
   depends_on "wxpython"
   depends_on "ffmpeg"
 
