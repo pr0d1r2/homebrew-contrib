379bf5db36626458d603ef278f5e4a3ecdf39899
Marcin Nowicki <pr0d1r2@gmail.com>
2015-05-26 06:54:52 +0200

    Experimenting with app created from setup.py in project.

--- youtube-dl-gui.rb
+++ youtube-dl-gui.rb
@@ -1,10 +1,11 @@
 class YoutubeDlGui < Formula
   homepage "https://github.com/MrS0m30n3/youtube-dl-gui"
-  url "https://github.com/MrS0m30n3/youtube-dl-gui.git",
-    :revision => "36c8147a0ba4da01215d92957246527285cca710"
+  url "https://github.com/pr0d1r2/youtube-dl-gui.git",
+    :branch => "osx-app",
+    :revision => "eb22a84759872bc6e946d261a9e0ac6a7ad45363"
 
   # cannot use git tag right now as new version with OSX support not released
-  version "0.3.7"
+  version "0.3.8"
 
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "wxpython"
@@ -13,12 +14,5 @@ class YoutubeDlGui < Formula
   def install
     ENV.prepend_create_path "PYTHONPATH", "#{lib}/python2.7/site-packages"
     system "python", "setup.py", "install", "--prefix=#{prefix}"
-    system "mkdir", "-p", "#{prefix}/YoutubeDlGui.app/Contents/MacOS"
-    system "cp", "#{prefix}/bin/youtube-dl-gui", "#{prefix}/YoutubeDlGui.app/Contents/MacOS/YoutubeDlGui"
-    system "mkdir", "-p", "#{prefix}/YoutubeDlGui.app/Contents/Resources/"
-    system "cp", "osx/YoutubeDlGui.icns", "#{prefix}/YoutubeDlGui.app/Contents/Resources/YoutubeDlGui.icns"
-    system "cp", "osx/empty.lproj", "#{prefix}/YoutubeDlGui.app/Contents/Resources/empty.lproj"
-    system "cp", "osx/Info.plist", "#{prefix}/YoutubeDlGui.app/Contents/Info.plist"
-    system "cp", "osx/PkgInfo", "#{prefix}/YoutubeDlGui.app/Contents/PkgInfo"
   end
 end
