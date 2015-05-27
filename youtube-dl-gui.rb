f1901926dd3c29a075f574035f27c75bdd8edf1d
Marcin Nowicki <pr0d1r2@gmail.com>
2015-05-27 17:12:46 +0200

    Build OSX app proper way.

--- youtube-dl-gui.rb
+++ youtube-dl-gui.rb
@@ -2,7 +2,7 @@ class YoutubeDlGui < Formula
   homepage "https://github.com/MrS0m30n3/youtube-dl-gui"
   url "https://github.com/pr0d1r2/youtube-dl-gui.git",
     :branch => "osx-app",
-    :revision => "eb22a84759872bc6e946d261a9e0ac6a7ad45363"
+    :revision => "a61d117bd4d925edf465d0fb6dd9f6ea282c2f79"
 
   # cannot use git tag right now as new version with OSX support not released
   version "0.3.8"
@@ -10,9 +10,11 @@ class YoutubeDlGui < Formula
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "wxpython"
   depends_on "ffmpeg"
+  depends_on "py2app"
 
   def install
     ENV.prepend_create_path "PYTHONPATH", "#{lib}/python2.7/site-packages"
-    system "python", "setup.py", "install", "--prefix=#{prefix}"
+    system "python", "setup.py", "py2app"
+    system "cp", "-r", "dist/Youtube-DLG.app", "#{prefix}/YoutubeDlGui.app"
   end
 end
