475f02809bc310051a4cf7fe4cac3f23a0e020fc
Marcin Nowicki <pr0d1r2@gmail.com>
2015-06-16 08:54:46 +0200

    Use self contained installer.

--- youtube-dl-gui.rb
+++ youtube-dl-gui.rb
@@ -12,9 +12,35 @@ class YoutubeDlGui < Formula
   depends_on "wxpython"
   depends_on "wxmac"
   depends_on "ffmpeg"
-  depends_on "py2app"
+
+  resource "altgraph" do
+    url "https://pypi.python.org/packages/source/a/altgraph/altgraph-0.12.tar.gz"
+    sha256 "b90db0dba36d9ece282b6a95ae3d324b63239694ce2cf2fd07d3efd7f2f7cab2"
+  end
+
+  resource "macholib" do
+    url "https://pypi.python.org/packages/source/m/macholib/macholib-1.7.tar.gz"
+    sha256 "1865bed8c50131649653d82cd1fbeb73a36a51355c48c81debdc195387b8103a"
+  end
+
+  resource "modulegraph" do
+    url "https://pypi.python.org/packages/source/m/modulegraph/modulegraph-0.12.1.tar.gz"
+    sha256 "8b278c56f962883986b1a3c8b963ace017553e66fce06a523b59c15c971971a3"
+  end
+
+  resource "py2app" do
+    url "https://pypi.python.org/packages/source/p/py2app/py2app-0.9.tar.gz"
+    sha256 "7922672f9e99b50ed931780d43405ac134988b1532dd0659ef130b824f88c59d"
+  end
 
   def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[altgraph modulegraph macholib py2app].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
     ENV.prepend_create_path "PYTHONPATH", "#{lib}/python2.7/site-packages"
     system "python", "setup.py", "py2app"
     cp_r "dist/Youtube-DLG.app", "#{prefix}/YoutubeDlGui.app"
